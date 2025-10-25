32ffb21ffd1471c124d4be2e7b77d71628136683
MatasKan13 <kantautasmatas@gmail.com>
2025-10-25 16:11:03 +0300

    Pridėta 3 strategija
    Pridėtos 3 strategijos funkcijos bei galimybė patikrinti išrūšiavimo tikslumą su užkomentuota funkcija „Spausdinimas“.

--- mylib.h
+++ mylib.h
@@ -2,7 +2,7 @@
 #include <iomanip>
 #include <vector>
 #include <string>
-#include <algorithm> // Skirta sort metodui
+#include <algorithm> // Skirta sort ir kietiems metodams
 #include <cctype> // Skirta toupper ir tolower metodams
 #include <random> // Skirta atsitiktinių skaičių generavimui
 #include <fstream> // Skirta darbui su failais
@@ -53,7 +53,32 @@ void Paskirstymas_vector_1_strategija(const vector <Studentas>&, const int&);
 void Paskirstymas_list_1_strategija(const list <Studentas>&, const int&);
 void Paskirstymas_vector_2_strategija(vector <Studentas>, const int&);
 void Paskirstymas_list_2_strategija(list <Studentas>, const int&);
+void Paskirstymas_vector_3_strategija(vector <Studentas>&, const int&);
+void Paskirstymas_list_3_strategija(list <Studentas>&, const int&);
 void Testavimas_vector(const int&, vector <Studentas>);
 void Testavimas_list(const int&, list <Studentas>);
 void Testavimas(int);
+
+// Galimam surūšiavimo į grupes patikrinimui:
+
+// template <typename T>
+// void Spausdinimas(T Spausd_gr) {
+//     stringstream ss;
+//     ss << setw(15) << left << "Vardas" << setw(20) << left << "Pavarde" << setw(17) << left << "Galutinis (Vid.) " << endl;
+//     ss << string(52,'-');
+//     for (const auto &Past : Spausd_gr) {
+//         ss << endl << setw(15) << left << Past.vardas << setw(20) << left << Past.pavarde << setw(17) << left << fixed << setprecision(2) << Past.gal;
+//     }
+    
+//     auto it = Spausd_gr.begin();
+//     if (it->gal >= 5) {
+//         ofstream out("kietiakai.txt");
+//         out << ss.str();
+//         out.close();
+//     } else {
+//         ofstream out("nuskriaustukai.txt");
+//         out << ss.str();
+//         out.close();
+//     }
+// }
 
