64a3cdb666dff15a9f06ff01b6279c21105ea2c3
MatasKan13 <kantautasmatas@gmail.com>
2025-10-29 17:09:07 +0200

    Testavimo funkcijos sukeltos į for ciklą dar trumpesniam kodui

--- main.cpp
+++ main.cpp
@@ -2,9 +2,7 @@
 
 int main() {
 
-    Testavimas(1'000);
-    Testavimas(10'000);
-    Testavimas(100'000);
-    Testavimas(1'000'000);
-    Testavimas(10'000'000);
+    for (int irasu_sk = 1'000; irasu_sk<=10'000'000; irasu_sk*=10) {
+        Testavimas(irasu_sk);
+    }
 }
