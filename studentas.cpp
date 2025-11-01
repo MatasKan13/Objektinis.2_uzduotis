927e61df3fb70c15f442816b4b0cb1b72d489360
MatasKan13 <kantautasmatas@gmail.com>
2025-11-01 20:10:30 +0200

    Add files via upload

---
+++ studentas.cpp
@@ -0,0 +1,20 @@
+#include "studentas.h"
+
+Studentas::Studentas(istringstream& iss) {
+    int pazymys;
+    iss >> vardas_ >> pavarde_;
+    while(iss >> pazymys) {
+        paz_.push_back(pazymys);
+    }
+    egz_ = paz_.back();
+    paz_.pop_back();
+}
+
+double Studentas::balas() const {
+    int suma = 0, n = paz_.size();
+    for (int p : paz_) {
+        suma+=p;
+    }
+    double gal = 0.4 * double(suma)/double(n) + 0.6 * egz_;
+    return gal;
+}
