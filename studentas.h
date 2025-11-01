ab5fa735745cefabe060c4b53dc0b0752cc609b0
MatasKan13 <kantautasmatas@gmail.com>
2025-11-01 22:38:19 +0200

    Add files via upload

---
+++ studentas.h
@@ -0,0 +1,29 @@
+#ifndef STUDENTAS_H
+#define STUDENTAS_H
+
+#include <string>
+#include <vector>
+#include <sstream>
+
+using std::string;
+using std::vector;
+using std::istringstream;
+
+class Studentas{
+    string vardas_;
+    string pavarde_;
+    vector <int> paz_;
+    int egz_;
+    double gal_;
+
+    public:
+        Studentas() : egz_(0) {};
+        Studentas(istringstream& iss);
+        inline string vardas() const {return vardas_;}
+        inline string pavarde() const {return pavarde_;}
+        inline double balas() const {return gal_;};
+        double balo_sk() const;
+        ~Studentas() {};
+};
+
+#endif
