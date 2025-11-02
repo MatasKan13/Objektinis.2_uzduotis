1acac43e0d1063d2447c94e0a7739132408fb10d
MatasKan13 <kantautasmatas@gmail.com>
2025-11-02 12:47:32 +0200

    Duomenų keitimo metodas

--- studentas.cpp
+++ studentas.cpp
@@ -19,3 +19,11 @@ double Studentas::balo_sk() const {
     double gal = 0.4 * double(suma)/double(n) + 0.6 * egz_;
     return gal;
 }
+
+void Studentas::keistiDuomenis(string v, string p, vector <int> pz, int e) {
+    vardas_ = v;
+    pavarde_ = p;
+    paz_ = pz;
+    egz_ = e;
+    gal_ = balo_sk();
+}
