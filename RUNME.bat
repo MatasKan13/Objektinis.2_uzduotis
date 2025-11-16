91214f1b01f0c4b1d4d05e3cbd21fee0d0eef877
MatasKan13 <kantautasmatas@gmail.com>
2025-11-16 22:23:26 +0200

    Sutvarkyti automatinio paleidimo failai

--- RUNME.bat
+++ RUNME.bat
@@ -1,6 +1,8 @@
+rmdir /s /q build
 mkdir build
+robocopy . .\build stud1000.txt stud10000.txt stud100000.txt stud1000000.txt stud10000000.txt 
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 ninja
-v1_2.exe
+v1_5.exe
 pause
